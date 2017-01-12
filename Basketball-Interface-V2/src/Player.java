f5edfce601ea370abf620ecd9fd20a6a901aa1da
Adrien <adriennev.42@gmail.com>
2017-01-12 11:08:48 -0500

    Updated Player and Team Classes

--- Basketball-Interface-V2/src/Player.java
+++ Basketball-Interface-V2/src/Player.java
@@ -1,6 +1,28 @@
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class Player {
+    public void Player(String name, int age, int yearOfGrad){
+        setName(name);
+        setAge(age);
+        setYearOfGrad(yearOfGrad);
+    }
+    public void Player(String name, int age){
+        Calendar now = Calendar.getInstance();
+        int year = now.get(Calendar.YEAR);
+
+        setName(name);
+        setAge(age);
+        setYearOfGrad(year);
+    }
+    public void Player(String name){
+        Calendar now = Calendar.getInstance();
+        int year = now.get(Calendar.YEAR);
+
+        setName(name);
+        setAge(0);
+        setYearOfGrad(year);
+    }
 
     String name;
     public String getName(){
@@ -11,7 +33,23 @@ public class Player {
     }
 
     int age;
-    int yearOfGraduation;
+    public int getage(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    int yearOfGrad;
+    public int getYearOfGrad(){
+        return yearOfGrad;
+    }
+    public void setYearOfGrad(int yearOfGrad){
+        this.yearOfGrad = yearOfGrad;
+    }
 
     ArrayList<GameStats> statBlock = new ArrayList<>();
+    public void addStatBlock(GameStats statBlock){
+        this.statBlock.add(statBlock);
+    }
 }
--- Basketball-Interface-V2/src/Team.java
+++ Basketball-Interface-V2/src/Team.java
@@ -1,3 +1,31 @@
-public class Team {
+import java.util.ArrayList;
 
+public class Team {
+    public void Team(String name, String school){
+        setTeamName(name);
+        setSchool(school);
+    }
+    
+    String teamName;
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    String school;
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    ArrayList<Player> Players = new ArrayList<>();
+    public void addPlayer(Player newPlayer){
+        this.Players.add(newPlayer);
+    }
 }
